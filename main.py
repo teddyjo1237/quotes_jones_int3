e9838a34a95630ed4fd386f5dde58dd93718af2f
VincentP-EFREI <118808798+VincentP-EFREI@users.noreply.github.com>
2023-05-23 12:27:01 +0100

    Update main.py
    remove

--- main.py
+++ main.py
@@ -24,9 +24,6 @@ def main():
             break
         else:
             print("Invalid input")
-        elif choice == 4 :
-            count = int(input("Enter the number of quotes to display: "))
-            display_quotes(quotes, count)
 
 
 if __name__ == "__main__":
